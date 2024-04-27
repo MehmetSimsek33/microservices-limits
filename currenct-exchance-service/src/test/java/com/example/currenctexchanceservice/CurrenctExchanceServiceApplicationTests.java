645b4a4f44211fe91924e7fc6f9319626066308f
mehmetsimsek33 <mehmetsimsek3345@gmail.com>
2024-04-27 11:47:35 +0300

    add currently service

---
+++ currenct-exchance-service/src/test/java/com/example/currenctexchanceservice/CurrenctExchanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currenctexchanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenctExchanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
